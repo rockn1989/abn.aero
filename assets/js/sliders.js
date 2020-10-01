df68321a6c0cdb9d35b7c30dead85e8238f75b28
rockn1989 <gold_100@bk.ru>
2020-10-01 23:21:03 +0300

    adaptvie video slider

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -1,65 +1,68 @@
 $(function () {
-
 	/*______ Voyage slider ______*/
 
-	$('.slider-voyage .slider').slick({
+	$(".slider-voyage .slider").slick({
 		infinite: false,
 		dots: false,
 		arrows: false,
 		autoplay: false,
 		slidesToShow: 3,
 		slidesToScroll: 1,
-		lazyLoad: 'progressive',
+		lazyLoad: "progressive",
 		responsive: [
 			{
 				breakpoint: 960,
 				settings: {
 					slidesToShow: 3,
-				}
+				},
 			},
 			{
 				breakpoint: 768,
 				settings: {
 					slidesToShow: 2,
-				}
-			}
-		]
+				},
+			},
+		],
 	});
-
 
 	/*______ Video gallery ______*/
 
-	var videoGallery = $('.video-slider .slider').on('init', function (event, slick) {
-
+	var videoGallery = $(".video-slider .slider").on("init", function (
+		event,
+		slick
+	) {
 		$.each(slick.$slides, function (i, el) {
-
-			var $youtube = $(el).find('.youtube'),
-				source = "https://img.youtube.com/vi/" + $youtube.data('embed') + "/0.jpg",
+			var $youtube = $(el).find(".youtube"),
+				source =
+					"https://img.youtube.com/vi/" + $youtube.data("embed") + "/0.jpg",
 				image = new Image();
 
 			image.src = source;
-			image.addEventListener('load', function () {
+			image.addEventListener("load", function () {
 				$youtube.append(image);
 			});
 
-			$youtube.on('click', function (e) {
+			$youtube.on("click", function (e) {
 				e.preventDefault();
-				if ($youtube.parents('.slide').hasClass('slick-current')) {
-					var iframe = $('<iframe>', {
+				if ($youtube.parents(".slide").hasClass("slick-current")) {
+					var iframe = $("<iframe>", {
 						frameborder: 0,
-						allowfullscreen: '',
+						allowfullscreen: "",
 						autoplay: true,
-						src: "https://www.youtube.com/embed/" + $youtube.data('embed') + "?rel=0&showinfo=0&autoplay=1"
+						src:
+							"https://www.youtube.com/embed/" +
+							$youtube.data("embed") +
+							"?rel=0&showinfo=0&autoplay=1",
 					});
-					$youtube.html('');
-					$youtube.find('.play-btn').fadeOut('350');
-					$youtube.find('.slide__title').fadeOut('350');
-					$youtube.find('img').fadeOut('350');
-					$youtube.append(iframe);
-				};
-
+					//$youtube.html("");
+					$youtube.find(".play-btn").fadeOut("350");
+					$youtube.find(".youtube__title").fadeOut("350");
+					$youtube.find("img").fadeOut("350", function () {
+						$youtube.append(iframe);
+					});
+				}
 			});
-		})
+		});
 	});
 
 	videoGallery.slick({
@@ -69,14 +72,13 @@ $(function () {
 		arrows: true,
 		infinity: true,
 		centerMode: false,
-		lazyLoad: 'ondemand',
+		lazyLoad: "ondemand",
 		focusOnSelect: true,
 		vertical: false,
-		prevArrow: '<div class="btn-slide slick-prev"><i class="icon-arrow-left"></i></div>',
-		nextArrow: '<div class="btn-slide slick-next"><i class="icon-arrow-right"></i></div>',
+		prevArrow: $(this).find(".slide-prev"),
+		nextArrow: $(this).find(".slide-next"),
 		customPaging: function (slider, i) {
-			return '<button><svg viewBox="0 0 120 120" version="1.1"xmlns="http://www.w3.org/2000/svg" class="btn-circle"><circle style="fill: none; stroke: #ff9d00; stroke-width: 6px;" cx="60" cy="60" r="50"/></svg></button>';
+			return '<button><svg viewBox="0 0 120 120" version="1.1"xmlns="http://www.w3.org/2000/svg" class="btn-circle"><circle style="fill: none; stroke: #ffffff; stroke-width: 6px;" cx="60" cy="60" r="50"/></svg></button>';
 		},
 	});
-
 });
