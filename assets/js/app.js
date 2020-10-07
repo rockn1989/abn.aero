133758a54016285ef0837d8747d9f1b8461c8bcb
rockn1989 <gold_100@bk.ru>
2020-10-07 21:05:35 +0300

    адаптив+слайдеры

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,16 @@
 $(function () {
+	/*______ Header fixed animation ______*/
+
+	$(".header-fixed").addClass("visible");
+
+	/*______ Close offcanvas menu ______*/
+
+	$(".uk-nav a").on("click", function () {
+		UIkit.offcanvas($("#offcanvas-menu")).hide();
+		$(".js__toggle-state").removeClass("active");
+		$(".btn-menu__text").text("меню");
+	});
+
 	/*______ Lazy Load ______*/
 
 	$(".lazy").lazy({
@@ -7,15 +19,40 @@ $(function () {
 		effectTime: 1000,
 		threshold: 0,
 		visibleOnly: true,
-		placeholder: "../img/ajax-loader.gif",
 		onError: function (element) {
 			console.log("error loading " + element.data("src"));
 		},
 	});
 
-	const util = UIkit.util;
+	const hedFixed = document.querySelector(".header-fixed");
 
-	$(".route-img__map, .route-img").addClass("active");
+	const sections = [...document.querySelectorAll(".white-section")];
+
+	var options = {
+		root: null,
+		rootMargin: "0px",
+		threshold: 0.01,
+	};
+
+	var callback = function (entries, observer) {
+		entries.forEach((entry) => {
+			console.log(entry.rootBounds);
+			if (entry.isIntersecting) {
+				hedFixed.classList.add("red");
+			} else {
+				hedFixed.classList.remove("red");
+			}
+		});
+	};
+
+	var observer = new IntersectionObserver(callback, options);
+	sections.forEach((i) => {
+		observer.observe(i);
+	});
+});
+
+$(function () {
+	const util = UIkit.util;
 
 	util.on($("#offcanvas-menu"), "show", function () {
 		$(".js__toggle-state").addClass("active");
--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -6,24 +6,37 @@ $(function () {
 		dots: false,
 		arrows: false,
 		autoplay: false,
-		slidesToShow: 3,
+		slidesToShow: 1,
 		slidesToScroll: 1,
 		lazyLoad: "progressive",
+		variableWidth: true,
+		swipeToSlide: true,
+		centerPadding: "50px",
+		centerMode: true,
+		speed: 1200,
 		responsive: [
 			{
 				breakpoint: 960,
 				settings: {
-					slidesToShow: 3,
+					slidesToShow: 1,
 				},
 			},
 			{
 				breakpoint: 768,
 				settings: {
-					slidesToShow: 2,
+					slidesToShow: 1,
 				},
 			},
 		],
 	});
+
+	$(".slider-voyage .slide").on("click", function (e) {
+		e.preventDefault();
+		let slideIndex = $(this).index();
+		$(".slider-voyage .slider")[0].slick.slickGoTo(slideIndex);
+	});
+
+	/*______ Comments slider ______*/
 
 	$(".comments-slider .slider").slick({
 		infinite: false,
@@ -34,6 +47,7 @@ $(function () {
 		slidesToScroll: 1,
 		lazyLoad: "progressive",
 		variableWidth: true,
+		swipeToSlide: true,
 		customPaging: function (slider, i) {
 			return '<button><svg viewBox="0 0 120 120" version="1.1"xmlns="http://www.w3.org/2000/svg" class="btn-circle"><circle style="fill: none; stroke: #ffffff; stroke-width: 6px;" cx="60" cy="60" r="50"/></svg></button>';
 		},
