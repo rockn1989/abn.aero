c294e5d1f6d84030e82dea56e268aa3432ce568e
rockn1989 <gold_100@bk.ru>
2020-10-08 23:20:14 +0300

    fullpage test

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,40 +14,16 @@ $(function () {
 	/*______ Lazy Load ______*/
 
 	$(".lazy").lazy({
+		appendScroll: $("section.section"),
 		scrollDirection: "vertical",
 		effect: "fadeIn",
 		effectTime: 1000,
 		threshold: 0,
 		visibleOnly: true,
+
 		onError: function (element) {
 			console.log("error loading " + element.data("src"));
 		},
-	});
-
-	const hedFixed = document.querySelector(".header-fixed");
-
-	const sections = [...document.querySelectorAll(".white-section")];
-
-	var options = {
-		root: null,
-		rootMargin: "0px",
-		threshold: 0.01,
-	};
-
-	var callback = function (entries, observer) {
-		entries.forEach((entry) => {
-			console.log(entry.rootBounds);
-			if (entry.isIntersecting) {
-				hedFixed.classList.add("red");
-			} else {
-				hedFixed.classList.remove("red");
-			}
-		});
-	};
-
-	var observer = new IntersectionObserver(callback, options);
-	sections.forEach((i) => {
-		observer.observe(i);
 	});
 });
 
@@ -80,9 +56,35 @@ $(function () {
 	UIkit.scrollspy(video, { repeat: true, hidden: false });
 
 	util.on(video, "inview", function () {
-		video.play();
+		//video.play();
 	});
 	util.on(video, "outview", function () {
-		video.pause();
+		//video.pause();
+	});
+});
+
+$(function () {
+	$("#pagepiling").pagepiling({
+		sectionsColor: [
+			"transparent",
+			"#0365DF",
+			"#ffffff",
+			"#ffffff",
+			"#0365DF",
+			"#ffffff",
+		],
+		anchors: ["desc", "plan", "plain", "comments", "price", "map"],
+		menu: "#menu",
+		navigation: false,
+		afterRender: function () {
+			$("#pp-nav").addClass("custom");
+		},
+		afterLoad: function (anchorLink, index) {
+			if (index > 1) {
+				$("#pp-nav").removeClass("custom");
+			} else {
+				$("#pp-nav").addClass("custom");
+			}
+		},
 	});
 });
